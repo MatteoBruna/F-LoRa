018c8e54a92ed23f893bf1b905e31af0780c14ba
Stefano Borini <stefano.borini@gmail.com>
2018-11-23 23:25:27 +0000

    Added files

---
+++ arduino/Struct_send_mio_2DHT/radio.cpp
@@ -0,0 +1,9 @@
+/*
+void radio_init() {
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+#ifdef IS_RFM69HW_HCW
+  radio.setHighPower(); //must include this only for RFM69HW/HCW!
+#endif
+  radio.encrypt(KEY);
+}
+*/
