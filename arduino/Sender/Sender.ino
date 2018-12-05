5292be590fddea15a6ab57784ebbffb7bc7f0566
MatteoBruna <matteo.bruna@gmail.com>
2018-12-05 17:07:39 +0000

    heavily modified sender. Integrated Lowpower library and battery reading

--- arduino/Sender/Sender.ino
+++ arduino/Sender/Sender.ino
@@ -1,43 +1,53 @@
+//based on mailbox notifier
+
 #include <Adafruit_Sensor.h>
 #include <DHT.h> //basato su adafruit unified sensor....works but it's huuuuge!
 #include <DHT_U.h>
 #include <RFM69.h>
+#include <RFM69_ATC.h>
 #include <SPI.h>
 #include <SPIFlash.h>
 #include <LowPower.h>
 
 #include "includes/config.h"
 
-DHT_Unified dht(DHTPIN, DHTTYPE);
+DHT_Unified dht(DHTPIN, DHTTYPE);  //create dht object
 
-int TRANSMITPERIOD = 5000; //transmit a packet to gateway so often (in ms)
-byte sendSize=0;
-boolean requestACK = false;
-
-#ifdef ENABLE_ATC
+#ifdef ENABLE_ATC    //create radio object
   RFM69_ATC radio;
 #else
   RFM69 radio;
 #endif
 
-typedef struct {
-  //int           nodeId; //store this nodeId ...it's actually useless because RFM69 class already gets the node's ID
-  bool          motion; //flag if data was sent due to motion or as regular communication
-  int         temp;   //sensor readings. I declare them int --> they will be sent as integers using the *10/10 trick
-  int         relhum;
-  //int       moist; //soil moisture sensor
-  int         battlev; //measure battery level
-} Payload;
-Payload theData;
-long lastPeriod = -1;
+volatile boolean motionDetected=false;
+char sendBuf[61];
+byte sendLen;
+byte sendLoops=0;
+boolean requestACK = false;
+
+unsigned long MLO=0; //MotionLastObserved (ago, in ms)
+unsigned long now = 0, time=0, lastSend = 0;
+unsigned int batterylevel = 0;
+unsigned int temp, relhum; //global variables for DHT22 measured values
+unsigned int soilmoisture; 
 
 void setup() {
-  	// Initialize DHT device.
+  	// Initialize devices
     serial_init();
     led_init();
     dht_init();
 	  radio_init();
  // if (flash.initialize()) flash.sleep(); //if Moteino has FLASH-MEM, make sure it sleeps
+
+  radio.sleep();
+  
+  pinMode(BATT_MONITOR, INPUT);
+  pinMode(FEEDDHT, OUTPUT);
+  pinMode(FEEDMOIST, OUTPUT);
+  //setup interrupt pin for motion sensor to wake up microcontroller
+  pinMode(MOTION_PIN, INPUT);
+  //attachInterrupt(MOTION_IRQ, motionIRQ, RISING);
+
 }
 
 void radio_init() {
@@ -51,14 +61,6 @@ void radio_init() {
   radio.enableAutoPower(ATC_RSSI);
   DEBUGln("RFM69_ATC Enabled (Auto Transmission Control)\n");
 #endif
-
-    char buff[128];
-    snprintf(buff, 128, "Transmitting at %d Mhz...", (FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915));
-    Serial.println(buff);
-
-//setup interrupt pin for motion sensor to wake up microcontroller
-  pinMode(MOTION_PIN, INPUT);
-  attachInterrupt(MOTION_IRQ, motionIRQ, RISING);
 }
 
 void dht_init() {
@@ -66,99 +68,144 @@ void dht_init() {
 }
 
 void serial_init() {
-  	Serial.begin(SERIAL_BAUD);
+  #ifdef SERIAL_EN
+    Serial.begin(SERIAL_BAUD);
+  #endif  
 }
 
-void loop() {
-    //check for any received packets
-    if (radio.receiveDone()) {
-        Serial.print('[');
-        Serial.print(radio.SENDERID, DEC);
-        Serial.print("] ");
-        for (byte i = 0; i < radio.DATALEN; i++) {
-            Serial.print((char)radio.DATA[i]);
-        }
-        Serial.print("   [RX_RSSI:");
-        Serial.print(radio.readRSSI());
-        Serial.print("]");
+void motionIRQ()
+{
+  motionDetected=true;
+  DEBUGln("IRQ");
+}
 
-        if (radio.ACKRequested()) {
-          radio.sendACK();
-          Serial.print(" - ACK sent");
-          delay(10);
-        }
-        led_blink(5);
-        Serial.println();
+void loop() {  //voglio che mandi il read dei sensori una volta ogni tot e il movimento ogni volta che lo registra. Quando registra il movimento non deve leggere i sensori, cosi' risparmio energia
+  now = millis();
+  readBattery();
+
+  if (motionDetected && (time-MLO > DUPLICATE_INTERVAL))
+  {
+    DEBUG("MOTION");
+    MLO = time; //save timestamp of event
+    sprintf(sendBuf, "MOTION,%d", batterylevel);
+    sendLen = strlen(sendBuf); DEBUGln(sendBuf);
+    if (radio.sendWithRetry(GATEWAYID, sendBuf, sendLen))
+    {
+     DEBUGln("..OK");
+     #ifdef BLINK_EN
+       Blink(LED,3);
+     #endif
     }
+    else DEBUGln("..NOK");
+    radio.sleep();
+  }
+  else sendLoops++;
 
-    int currPeriod = millis()/TRANSMITPERIOD;   //meglio di "delay()"
-    if (currPeriod != lastPeriod) {
-        int temp, relhum;
-        dhtstuff(&temp, &relhum);
-
-        //fill in the struct with new values
-        //theData.nodeId = NODEID;
-        //theData.uptime = millis();
-        theData.temp = temp; //sensor data
-        theData.relhum = relhum;
-
-        Serial.print("Sending struct (");
-        Serial.print(sizeof(theData));
-        Serial.print(" bytes) ... ");
-        if (radio.sendWithRetry(GATEWAYID, (const void*)(&theData), sizeof(theData))) {
-            Serial.print(" ok!");
-        }
-        else {
-            Serial.print(" nothing...");
-        }
-        Serial.println();
-        led_blink(3);
-        lastPeriod=currPeriod;
-    }
+  //send sensor readings every SENDEVERYXLOOPS
+  if (sendLoops>=SENDEVERYXLOOPS)
+  {
+    dhtmeas(); //read the sensors
+    soilmoist();
+    
+    sendLoops=0;
+    char periodO='X';
+    unsigned long lastOpened = (time - MLO) / 1000; //get seconds
+    
+    if (lastOpened <= 59) periodO = 's'; //1-59 seconds
+    else if (lastOpened <= 3599) { periodO = 'm'; lastOpened/=60; } //1-59 minutes
+    else if (lastOpened <= 259199) { periodO = 'h'; lastOpened/=3600; } // 1-71 hours
+    else if (lastOpened >= 259200) { periodO = 'd'; lastOpened/=86400; } // >=3 days
+   
+    DEBUG("lastOpened: ");
+    DEBUGln(lastOpened);
+    DEBUG("periodO: ");
+    DEBUGln(periodO);
+    DEBUG("batterylevel: ");
+    DEBUGln(batterylevel);
+    DEBUG("temp: ");
+    DEBUGln(temp);
+    DEBUG("relhum: ");
+    DEBUGln(relhum);
+    DEBUG("soilmoisture: ");
+    DEBUGln(soilmoisture);
+            
+    sprintf(sendBuf, "%ld,%c,%d,%d,%d,%d", lastOpened, periodO, batterylevel, temp, relhum, soilmoisture); //es: 56,m,48,15,69,96
+    sendLen = strlen(sendBuf);
+    radio.send(GATEWAYID, sendBuf, sendLen);
+    radio.sleep();
+    DEBUG(sendBuf); DEBUG(" ("); DEBUG(sendLen); DEBUGln(")"); 
+    lastSend = time;
+    #ifdef BLINK_EN
+      Blink(3);
+    #endif
+  }
+  
+  motionDetected=false; //do NOT move this after the SLEEP line below or motion will never be detected
+  time = time + 8000 + millis()-now + 480; //correct millis() resonator drift, may need to be tweaked to be accurate
+  //LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  delay(5000);
+  DEBUGln("WAKEUP");
 }
 
 void led_init() {
     pinMode(LED, OUTPUT);//once deployed it's not useful...comment out
 }
 
-void led_blink(int DELAY_MS) {
+void Blink(int DELAY_MS) {
     digitalWrite(LED, HIGH);
     delay(DELAY_MS);
     digitalWrite(LED, LOW);
 }
 
 
-void dhtstuff(int* temp, int* relhum)
+void dhtmeas()
 { 
+unsigned int n = 0; //variable for repeating the measurements if necessary
+digitalWrite(FEEDDHT, HIGH); //feed the sensor from OUTPUT pin
+delay(1500); //the sensor needs time to warm up and give reliable measurements
+sensors_event_t event;
 
-sensors_event_t event;  
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    *temp = 999;
-  }
-  else {
-    *temp = (int) (event.temperature * 10);
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    *relhum = 999;
-  }
-  else {
-    *relhum = (int) (event.relative_humidity * 10);
-  }
+  // Get temperature event and print its value.
+  for (byte i=0; i<3; i++){
+    dht.temperature().getEvent(&event);
+    if (!isnan(event.temperature)) {
+      temp = (int) (event.temperature * 10);
+      break;
+      }
+     else {
+       temp = 999;
+      }
+    }
   
-  return;
+  // Get humidity event and print its value.
+  for (byte i=0; i<3; i++){
+    dht.humidity().getEvent(&event);
+    if (!isnan(event.relative_humidity)) {
+      relhum = (int) (event.relative_humidity * 10);
+      break;
+      }
+     else {
+       relhum = 999;
+      }
+    }
+
+  digitalWrite(FEEDDHT, LOW); //switch off sensor
   }
 
-//  void readBattery(int* batlev)
-//{
-//  unsigned int readings=0;
-//
-//  for (byte i=0; i<5; i++) //take several samples, and average
-//    readings+=analogRead(BATT_MONITOR);
- 
-//  batteryVolts = BATT_FORMULA(readings / 5.0);
-//  dtostrf(batteryVolts,3,2, BATstr); //update the BATStr which gets sent every BATT_CYCLES or along with the MOTION message
-//  if (batteryVolts <= BATT_LOW) BATstr = "LOW";
-//}
+void readBattery()
+{
+  unsigned int readings=0;
+  for (byte i=0; i<5; i++) //take several samples, and average
+    readings+=analogRead(BATT_MONITOR);
+    batterylevel = (int) ((readings / 5.0)*16.4)-906;
+}
+
+void soilmoist()
+{
+  digitalWrite(FEEDMOIST, HIGH); //feed the sensor
+  unsigned int readings=0;
+  for (byte i=0; i<5; i++) //take several samples, and average
+    readings+=analogRead(MOISTPIN);
+    soilmoisture = readings / 5;
+  digitalWrite(FEEDMOIST, LOW); //feed the sensor
+}
