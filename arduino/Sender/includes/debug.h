f55d134c2aa3db690f293e257e85b07ce82da378
Stefano Borini <stefano.borini@gmail.com>
2018-12-11 22:27:08 +0000

    Extracted debug functionality

---
+++ arduino/Sender/includes/debug.h
@@ -0,0 +1,11 @@
+#ifdef SERIAL_EN
+#define DEBUG(input)   {Serial.print(input); delay(1);}
+#define DEBUGln(input) {Serial.println(input); delay(1);}
+#define DEBUGFlush() { Serial.flush(); }
+#else
+#define DEBUG(input);
+#define DEBUGln(input);
+#define DEBUGFlush();
+#endif
+
+
